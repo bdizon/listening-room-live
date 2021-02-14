c1c616a76596c65251ed03d9d1431fa69a8f255c
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-02-14 16:26:42 -0600

    Delete room_controller directory

--- room_controller/frontend/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# render index template then let react take care of whats inside of index.html
-def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
--- room_controller/spotify/credentials.py
+++
@@ -1,4 +0,0 @@
-# store spotify credentials: client id, client secret, redirect url
-CLIENT_ID = "e92af545078c414a88ea1237b3a73793"
-CLIENT_SECRET = "dc599abfcea14095abb7a84e6e54a7e8"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
