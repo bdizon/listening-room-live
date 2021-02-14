5a750ff3748bdb5c3654573d2b0071d392904390
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-02-14 16:27:26 -0600

    Add files via upload

---
+++ spotify/credentials.py
@@ -0,0 +1,4 @@
+# store spotify credentials: client id, client secret, redirect url
+CLIENT_ID = "e92af545078c414a88ea1237b3a73793"
+CLIENT_SECRET = "dc599abfcea14095abb7a84e6e54a7e8"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
