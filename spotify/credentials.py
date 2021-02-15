9569335e852eb8fdea9adb5a763f2b15eebf2730
Bianca Dizon <59716901+bdizon@users.noreply.github.com>
2021-02-15 13:59:04 -0600

    changed redirect_uri

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,5 @@
 # store spotify credentials: client id, client secret, redirect url
 CLIENT_ID = "e92af545078c414a88ea1237b3a73793"
 CLIENT_SECRET = "dc599abfcea14095abb7a84e6e54a7e8"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+# REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "https://pandemic-party.herokuapp.com/spotify/redirect "
